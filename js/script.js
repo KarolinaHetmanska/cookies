123db1ece22408fcbd54e1de9ee04b21f9449da9
Karolina Hetmanska <khetmanska@k85.pl>
2017-04-20 22:47:09 +0200

    cookie, gotowe

--- js/script.js
+++ js/script.js
@@ -1,64 +1,38 @@
-function setCookie()
-{
-  var tempObject = {};
-  tempObject.name = document.getElementById('input-name').value
-  tempObject.season = document.getElementById('dropdown-favourite-season').value
+$(document).ready(function () {
+  $('#save-cookie').click(function setCookie() {
+    var tempObject = {};
+    tempObject.name = $('#input-name').val()
+    tempObject.season = $('#dropdown-favourite-season').val()
 
-  var jsonString = JSON.stringify(tempObject)
-  document.cookie = "cookieObject=" + jsonString +"; max-age=" + (60*60*24) + ";"
+    var jsonString = JSON.stringify(tempObject)
+    document.cookie = "cookieObject=" + jsonString + "; max-age=" + (60 * 60 * 24) + ";"
+  })
 
-  // var inputValue = document.getElementById('example-input').value
-  // if (inputValue != "") {
-  //    document.cookie = "name=" + inputValue + ";max-age=" + (60*60*24) + ";"
-  // }
-}
-
-$(document).ready(function getCookie() {
-  if (document.cookie.length != 0) {
-    console.log("document.cookie =", document.cookie);
+  $(document).ready(function getCookie() {
+    if (document.cookie.length != 0) {
+      console.log("document.cookie =", document.cookie);
       var cookieValueArray = document.cookie.split(";")
 
       function includesCookie(someArrayItem) {
         if (someArrayItem.includes('cookieObject')) {
-            return someArrayItem
+          return someArrayItem
         }
       }
+      var cookieValueArraySplited = cookieValueArray.find(includesCookie).split("=")
+      var cookieValueFinalObject = JSON.parse(cookieValueArraySplited[1])
 
-      var cookieValueArrayReady = cookieValueArray.find(includesCookie).split("=")
+      if ((cookieValueFinalObject.name.length != 0) && (cookieValueFinalObject.season != 'wybierz')) {
+        console.log('name is not zero')
 
-      var cookieValueObject = JSON.parse(cookieValueArrayReady[1])
-      // tempObject.name = document.getElementById('input-name').value
-      console.log(cookieValueObject.name);
-      // tempObject.season = document.getElementById('dropdown-favourite-season').value
-      console.log(cookieValueObject.season);
-
-    if (cookieValueObject.name.length != 0) {
-      console.log('name is not zero')
-
-      $('#empty-cookies').addClass('non-display');
-      $('#notempty-cookies').removeClass('non-display').addClass('display');
-      $('#cookie-name').html(cookieValueObject.name)
-    }
-
-    if (cookieValueObject.season != 'select') {
-      if (cookieValueObject.season == 'spring') {
-        $('#notempty-cookies').addClass('spring')
-      }
-      else {
-        $('#notempty-cookies').addClass('autumn')
-
+        $('#if-empty-cookies').addClass('non-display')
+        $('#if-notempty-cookies').removeClass('non-display').addClass('display')
+        $('#cookie-name').text(cookieValueFinalObject.name)
+        $('#cookie-season').text(cookieValueFinalObject.season)
+        $('#cookie-season').addClass(cookieValueFinalObject.season);
+      } else {
+        $('#missed-information').addClass('display')
+        $('#missed-information').text('Nie podaleś wszystkich informacji')
       }
     }
-
-
-  }
-
-  // if (document.cookie.length != 0) {
-  //   var cookieValueArray = document.cookie.split("=")
-  //   alert('hello, this is my first cookie :DD and its name is: ' + cookieValueArray[0] + 'and value is: ' + cookieValueArray[1])
-  // }
-  // else {
-  //   alert('sorry, there is no cookie :(')
-  // }
-}
-)
+  })
+})
